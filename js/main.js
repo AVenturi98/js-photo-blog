f346ec7a071230c45d7403dcef40a0347f39afc6
AVenturi98 <lssndrventuri@gmail.com>
2024-11-08 09:25:23 +0100

    excercise #1

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSO
 Bonus
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
 
+//-------------
 CONSEGNA #2
 
 Milestone 1
